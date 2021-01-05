2874d20120843e7ad19907e0f8ea308ea8ce6227
Kaido Järvemets <me@kaidojarvemets.ee>
2021-01-05 15:47:27 +0200

    Update Update incident details.ps1

--- EBook Examples/Incident Management/Update incident details.ps1
+++ EBook Examples/Incident Management/Update incident details.ps1
@@ -3,11 +3,17 @@ Connect-AzureAD
 
 $AzureADUserDetails = Get-AzureADUser -ObjectId "John@Contoso.com"
 $IncidentID = "499d8110-790e-43d9-a9d9-a15f0539fcf0"
+
 $IncidentOwnerDetails = @{
         AssignedTo = $AzureADUserDetails.DisplayName
         Email = $AzureADUserDetails.Mail
         Objectid = $AzureADUserDetails.ObjectId
         UserPrincipalName = $AzureADUserDetails.UserPrincipalName
+}
+
+$AzureSentinelWorkSpaceInfo = @{
+    ResourceGroupName = "RG-PROD-IT-AZ-MANAGEMENT-TIER-0-WE"
+    WorkspaceName = "LF-TIER-0-LOG-ANALYTICS-WE"
 }
 
 $IncidentOwner = New-AzSentinelIncidentOwner @IncidentOwnerDetails

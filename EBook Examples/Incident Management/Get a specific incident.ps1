42d031743d79cd22e9b2a1c436c85f58dba22976
Kaido Järvemets <me@kaidojarvemets.ee>
2021-01-05 15:48:25 +0200

    Update Get a specific incident.ps1

--- EBook Examples/Incident Management/Get a specific incident.ps1
+++ EBook Examples/Incident Management/Get a specific incident.ps1
@@ -1,3 +1,8 @@
 ﻿#Get a specific incident
+ $AzureSentinelWorkSpaceInfo = @{
+    ResourceGroupName = "RG-PROD-IT-AZ-MANAGEMENT-TIER-0-WE"
+    WorkspaceName = "LF-TIER-0-LOG-ANALYTICS-WE"
+}
+
 $IncidentID = "499d8110-790e-43d9-a9d9-a15f0539fcf0"
-Get-AzSentinelIncident @AzInfo -IncidentId $IncidentID
+Get-AzSentinelIncident @AzureSentinelWorkSpaceInfo -IncidentId $IncidentID

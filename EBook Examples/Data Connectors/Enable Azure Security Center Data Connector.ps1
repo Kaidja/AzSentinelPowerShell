28a89512ddadcdb1fa7db9dcca760819150e1c7c
Kaido Järvemets <me@kaidojarvemets.ee>
2021-01-07 22:08:16 +0200

    Add files via upload

---
+++ EBook Examples/Data Connectors/Enable Azure Security Center Data Connector.ps1
@@ -0,0 +1,5 @@
+﻿$AzureSentinelWorkSpaceInfo = @{
+    ResourceGroupName = "RG-PROD-IT-AZ-MANAGEMENT-TIER-0-WE"
+    WorkspaceName = "LF-TIER-0-LOG-ANALYTICS-WE"
+}    
+New-AzSentinelDataConnector @AzureSentinelWorkSpaceInfo -AzureSecurityCenter -SubscriptionId "%YOURSUBSCRIPTIONID%" -Alerts Enabled
---
+++ EBook Examples/Data Connectors/Get Data Connectors.ps1
@@ -0,0 +1,6 @@
+﻿$AzureSentinelWorkSpaceInfo = @{
+    ResourceGroupName = "RG-PROD-IT-AZ-MANAGEMENT-TIER-0-WE"
+    WorkspaceName = "LF-TIER-0-LOG-ANALYTICS-WE"
+}    
+Get-AzSentinelDataConnector @AzureSentinelWorkSpaceInfo |
+    Select-Object -Property Kind,Name

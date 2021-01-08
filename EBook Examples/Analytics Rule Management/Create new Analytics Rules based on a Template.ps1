205aef4944a82f8de3fab5102eb2e5a3fe52f6f8
Kaido Järvemets <me@kaidojarvemets.ee>
2021-01-08 16:06:24 +0200

    Add files via upload

---
+++ EBook Examples/Analytics Rule Management/Create new Analytics Rules based on a Template.ps1
@@ -0,0 +1,25 @@
+﻿$AzureSentinelWorkSpaceInfo = @{
+    ResourceGroupName = "RG-PROD-IT-AZ-MANAGEMENT-TIER-0-WE"
+    WorkspaceName = "LF-TIER-0-LOG-ANALYTICS-WE"
+}
+
+$AnalyticsRule = Get-AzSentinelAlertRuleTemplate @AzureSentinelWorkSpaceInfo | 
+    Where-Object {$PSItem.DisplayName -eq "Rare application consent"}
+
+#Scheduled based Template info
+
+$ScheduledBasedTemplateInfo = @{
+    AlertRuleTemplateName = $AnalyticsRule.Name
+    Enabled = $True
+    DisplayName = $AnalyticsRule.DisplayName
+    Scheduled = $True
+    Query = $AnalyticsRule.Query
+    QueryFrequency = $AnalyticsRule.QueryFrequency
+    QueryPeriod = $AnalyticsRule.QueryPeriod
+    Severity = $AnalyticsRule.Severity
+    TriggerThreshold = $AnalyticsRule.TriggerThreshold
+    TriggerOperator = $AnalyticsRule.TriggerOperator
+
+}
+
+New-AzSentinelAlertRule @AzureSentinelWorkSpaceInfo @ScheduledBasedTemplateInfo

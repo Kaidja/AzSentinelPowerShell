315c72cb59f49b3a838173581a01aa2adf0498c8
Kaido Järvemets <me@kaidojarvemets.ee>
2021-01-08 15:38:41 +0200

    Add files via upload

---
+++ EBook Examples/Analytics Rule Management/Export All Analytics Rule Templates to a CSV File.ps1
@@ -0,0 +1,7 @@
+﻿$AzureSentinelWorkSpaceInfo = @{
+    ResourceGroupName = "RG-PROD-IT-AZ-MANAGEMENT-TIER-0-WE"
+    WorkspaceName = "LF-TIER-0-LOG-ANALYTICS-WE"
+}
+
+Get-AzSentinelAlertRuleTemplate @AzureSentinelWorkSpaceInfo | Select-Object -Property DisplayName,Name,CreatedDateUtc,Severity | 
+    Export-Csv -Path "$($ENv:USERPROFILE)\Desktop\AzureSentinelAnalyticsRules.csv" -NoTypeInformation -UseCulture

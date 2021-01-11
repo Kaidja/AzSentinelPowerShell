a1863fe8c205931fc5c4335225aff19dba0bc89f
Kaido Järvemets <me@kaidojarvemets.ee>
2021-01-11 21:36:55 +0200

    Add files via upload

---
+++ EBook Examples/Analytics Rule Management/Export All Analytics Rules to JSON.ps1
@@ -0,0 +1,33 @@
+﻿#Connect to Azure 
+Connect-AzAccount
+
+#Run the Set-AzContext only, if you have multiple subscriptions
+#Set-AzContext -Subscription "YOUR SUBSCRIPTION ID"
+
+#Define the Azure Sentinel workspace information
+$AzureSentinelWorkSpaceInfo = @{
+    ResourceGroupName = "RG-PROD-IT-AZ-MANAGEMENT-TIER-0-WE"
+    WorkspaceName = "LF-TIER-0-LOG-ANALYTICS-WE"
+}
+
+$Rules = Get-AzSentinelAlertRuleTemplate @AzureSentinelWorkSpaceInfo
+
+foreach($Rule in $Rules){
+    
+    #Properties to export
+    $TemplateInfo = @{
+        AlertRuleTemplateName = $Rule.Name
+        DisplayName = $Rule.DisplayName
+        Query = $Rule.Query
+        QueryFrequency = $Rule.QueryFrequency
+        QueryPeriod = $Rule.QueryPeriod
+        Severity = $Rule.Severity
+        TriggerThreshold = $Rule.TriggerThreshold
+        TriggerOperator = $Rule.TriggerOperator
+        Kind = $Rule.Kind
+    }
+
+    $JSON = $TemplateInfo | ConvertTo-Json
+    $JSON | Out-File -FilePath "($env:USERPROFILE)\Desktop\$($TemplateInfo.DisplayName).json"
+
+}
